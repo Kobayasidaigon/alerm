af110e838ec89487a3544221c100633eaff0146b
Kobayashidaigon <hal.60445.kobayashi@gmail.com>
2020-06-27 14:36:15 +0900

    setintervalのサンプル作成

--- alerm.js
+++ alerm.js
@@ -1,10 +1,16 @@
 const date = new Date();
 const decideTimeButton = document.querySelector(".decideTimeButton");
-
 decideTimeButton.addEventListener("click", function () {
-    //アラームをかける時間を取得
-    let alermTime = document.querySelector("input[name='time']").value;
-    let time = alermTime.split(':');
+  //アラームをかける時間を取得
+  let decideTime = document.querySelector("input[name='time']").value;
+  let time = decideTime.split(":");
+
+  
+  var tm = 1000;
+  set = setInterval(fn, tm);
 });
-let nowHH = date.getHours();
-let nowMM = date.getMinutes();
+
+var fn = function () {
+  console.log("10秒経過しました");
+};
+
