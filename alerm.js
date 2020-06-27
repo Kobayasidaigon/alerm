ae2484b164b69c193ddf42e6656eb96317368c1d
Kobayashidaigon <hal.60445.kobayashi@gmail.com>
2020-06-27 14:42:01 +0900

    初期表示で現在時刻を表示

--- alerm.js
+++ alerm.js
@@ -5,7 +5,6 @@ decideTimeButton.addEventListener("click", function () {
   let decideTime = document.querySelector("input[name='time']").value;
   let time = decideTime.split(":");
 
-  
   var tm = 1000;
   set = setInterval(fn, tm);
 });
@@ -14,3 +13,10 @@ var fn = function () {
   console.log("10秒経過しました");
 };
 
+//初期表示で現在時刻を表示
+window.onload = function () {
+  let firstDisplayTime = document.querySelector("input[name='time']");
+  const firstDisplayTimeHH = date.getHours();
+  const firstDisplayTimeMM = date.getMinutes();
+  firstDisplayTime.value = firstDisplayTimeHH + ":" + firstDisplayTimeMM;
+};
