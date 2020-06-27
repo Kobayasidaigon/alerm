37eb1ada3a856e6975f66f490cbf0bc792bd707d
Kobayashidaigon <hal.60445.kobayashi@gmail.com>
2020-06-27 15:10:16 +0900

    表示時刻、時間経過とともに変化

--- alerm.js
+++ alerm.js
@@ -1,4 +1,4 @@
-const date = new Date();
+
 const decideTimeButton = document.querySelector(".decideTimeButton");
 decideTimeButton.addEventListener("click", function () {
   //アラームをかける時間を取得
@@ -15,8 +15,28 @@ var fn = function () {
 
 //初期表示で現在時刻を表示
 window.onload = function () {
-  let firstDisplayTime = document.querySelector("input[name='time']");
-  const firstDisplayTimeHH = date.getHours();
-  const firstDisplayTimeMM = date.getMinutes();
-  firstDisplayTime.value = firstDisplayTimeHH + ":" + firstDisplayTimeMM;
+  displayTime();
+};
+
+setInterval(() => {
+  displayTime();
+}, 60000);
+
+//画面に表示される時間を取得、反映
+function displayTime() {
+  const date = new Date();
+  let DisplayTime = document.querySelector("input[name='time']");
+  const DisplayTimeHH = toDoubleDigits(date.getHours());
+  const DisplayTimeMM = toDoubleDigits(date.getMinutes());
+  DisplayTime.value = DisplayTimeHH + ":" + DisplayTimeMM;
+  document.querySelector(".now-time").textContent = DisplayTime.value;
+}
+
+//時、分が一桁の時０を追加する
+var toDoubleDigits = function (num) {
+  num += "";
+  if (num.length === 1) {
+    num = "0" + num;
+  }
+  return num;
 };
